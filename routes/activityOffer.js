51299a82eedd0b01df3900e4a7c04ad6c6a73178
VCheng <vcheng@internal.salesforce.com>
2016-04-08 13:06:40 +1000

    test

--- routes/activityOffer.js
+++ routes/activityOffer.js
@@ -20,6 +20,7 @@ exports.save = function( req, res ) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
 
+	console.log("SAVE()|TOKEN = ", req.session.token);
     activityUtils.logData( req );
 		
     res.send( 200, 'Save' );
